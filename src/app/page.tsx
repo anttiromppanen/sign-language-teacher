9d9998a86eb548b4601f3fdaa8278286883cf62a
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-25 21:59:59 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,17 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import * as tf from "@tensorflow/tfjs";
-import * as handdetection from "@tensorflow-models/hand-pose-detection";
-import { Hands } from "@mediapipe/hands";
+import { useRef } from "react";
 import Webcam from "react-webcam";
 
 export default function Home() {
-  const [handDetector, setHandDetector] = useState(null);
   const cameraRef = useRef<Webcam | null>(null);
-
-  useEffect(() => {
-    const loadModel = async () => {
-      if (!cameraRef.current) return;
-
-      const model = await tf.loadGraphModel("/models/model.json");
-      const hands = new Hands();
-
-      hands.setOptions({
-        maxNumHands: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
-
-      const detector = await handdetection.createDetector(
-        handdetection.SupportedModels.MediaPipeHands,
-        {
-          runtime: "tfjs",
-        }
-      );
-      console.log(detector);
-    };
-    loadModel();
-  }, []);
 
   return (
     <div>
       <Webcam
         ref={cameraRef}
         audio={false}
+        mirrored
         videoConstraints={{
           width: 640,
           height: 480,
