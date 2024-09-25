9a99502841a920f25602a562ff7ef28a744dee0e
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-26 02:51:37 +0300

    load model test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Webcam from "react-webcam";
+import { FilesetResolver, HandLandmarker } from "@mediapipe/tasks-vision";
 
 export default function Home() {
   const cameraRef = useRef<Webcam | null>(null);
+
+  async function initializeHandLandmarker() {
+    const vision = await FilesetResolver.forVisionTasks(
+      // path/to/wasm/root
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+    const handLandmarker = await HandLandmarker.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath:
+          "/models/sign_gesture_recognizer/sign_gesture_recognizer.task",
+        delegate: "GPU",
+      },
+      numHands: 1,
+      runningMode: "VIDEO",
+    });
+    return handLandmarker;
+  }
+
+  useEffect(() => {
+    (async () => {
+      const handLandmarker = await initializeHandLandmarker();
+      console.log("HandLandmarker initialized", handLandmarker);
+    })();
+  }, []);
 
   return (
     <div>
