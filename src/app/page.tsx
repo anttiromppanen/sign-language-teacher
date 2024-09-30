9d9726f35cdb1d5e14baa1a2b081beca0f9cf0b6
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-30 08:14:38 +0300

    update landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,12 @@ export default function Home() {
     <div>
       <div className="flex flex-col gap-y-14 items-center h-screen w-full text-center px-8">
         <h1 className="text-4xl sm:text-5xl md:text-6xl mt-10 text-foreground">
-          Sign Language Alphabet
+          Learn Sign Language Effortlessly Anytime, Anywhere
         </h1>
+        <h2>
+          Master American Sign Language (ASL) alphabet with interactive learning
+          tool using your device&apos;s camera and AI
+        </h2>
         <GestureSlideshow />
       </div>
     </div>
