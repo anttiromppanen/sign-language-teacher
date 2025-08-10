ed58e3c12501bf8ae344a7b915d438c4f9f21917
Antti Romppanen <romppanen.antti@gmail.com>
2025-08-10 16:13:00 +0300

    /alphabet ui update

--- src/app/alphabet/page.tsx
+++ src/app/alphabet/page.tsx
@@ -2,8 +2,8 @@ import GestureSlideshow from "@/components/GestureSlideshow";
 
 function Alphabet() {
   return (
-    <main className="flex flex-col items-center justify-center max-w-4xl mx-auto text-center py-10">
-      <h1 className="text-6xl text-foreground mb-10 font-alice">
+    <main className="flex flex-col items-center justify-center max-w-[1500px] mx-auto text-center py-10">
+      <h1 className="text-7xl text-foreground mb-10 font-alice">
         The American Sign Language (ASL) alphabet
       </h1>
       <GestureSlideshow />
