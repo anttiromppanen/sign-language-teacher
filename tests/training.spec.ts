b1e812c7cc036464949c378371cc1ab4dd3a7b5a
Antti Romppanen <romppanen.antti@gmail.com>
2025-08-15 12:39:38 +0300

    update test

--- tests/training.spec.ts
+++ tests/training.spec.ts
@@ -1,21 +1,14 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test.describe("/training", () => {
-	test("should show camera error when webcam is not detected", async ({
-		page,
-	}) => {
+	test("should show correct overlay when entered", async ({ page }) => {
 		await page
 			.context()
 			.grantPermissions([], { origin: "http://localhost:3000" });
 
 		await page.goto("http://localhost:3000/training");
-		await page.click("text=Start learning now!");
-		await page.getByTestId("image-overlay-button").click();
-
-		const cameraErrorElement = page.getByRole("heading", {
-			name: "Error: No camera detected",
-		});
-
-		await expect(cameraErrorElement).toBeVisible(); // Ensure that the CameraError component appears
+		await expect(
+			page.getByRole("heading", { name: "Before we get started" }),
+		).toBeVisible();
 	});
 });
